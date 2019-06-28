a243fc8661f7233a75a7aa595c76ef45a78090bb
Daniel Isaksen <d@duniel.no>
2019-06-28 22:08:59 +0200

    fix up flyspell

--- rc.d/60-flyspell.el
+++ rc.d/60-flyspell.el
@@ -1,3 +1,5 @@
 ;; Flyspell for code comments.
-;; (add-hook 'erlang-mode-hook   'flyspell-prog-mode)
-;; (add-hook 'c-mode-common-hook 'flyspell-prog-mode)
+(add-hook 'erlang-mode-hook   'flyspell-prog-mode)
+(add-hook 'c-mode-common-hook 'flyspell-prog-mode)
+(add-hook 'org-mode-hook      'flyspell-mode)
+
