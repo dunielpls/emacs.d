1414908957f8cb0e67c7029f36b9f0e49d9f38d4
Daniel Isaksen <d@duniel.no>
2019-07-24 03:03:38 +0200

    moved some theme stuff to the correct file, and apparently distel changes?

--- rc.d/130-themes.el
+++ rc.d/130-themes.el
@@ -1,1 +1,24 @@
+;; Theme stuffs.
+(require 'rand-theme)
+(setq rand-theme-wanted '(
+                          xemacs
+                          adwaita
+                          gnome
+                          plan9
+                          github-modern
+                          one-dark
+                          one-light
+                          midnight
+                          zenburn
+                          spolsky
+                          borland-bluex
+                          ))
 
+;; Set the default theme.
+;; (rand-theme)
+;; (load-theme 'xemacs t)
+(if (display-graphic-p)
+    (load-theme 'spolsky t)
+  (load-theme 'dracula t))
+
+(set-face-attribute 'fringe nil :background nil)
